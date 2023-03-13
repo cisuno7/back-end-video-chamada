942483ca062d07f96396850569ed128f146164ae
cisuno7 <cisuno7@hotmail.com>
2023-03-13 10:44:29 -0300

    Cors Erro Localhost

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const app = express();
 const route = require('./routes')
 const cors = require('cors');
 
-app.use(cors('*'));
+const corsOptions = {
+    origin: 'http://localhost:19006/',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:19006");
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
 app.use(express.json());
 app.use(route);
 
