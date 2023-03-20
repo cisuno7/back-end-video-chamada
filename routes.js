a6f8ddca10b3453355f611da4d67580e27768e1c
cisuno7 <cisuno7@hotmail.com>
2023-03-20 10:29:57 -0300

    tratamentos tirados da api

--- routes.js
+++ routes.js
@@ -17,15 +17,15 @@ route.get('/users', (req, res) => {
 route.post('/users', (req, res) => {
     const { username, password,email,nickname } = req.body;
 
-    if(!username){
+    if(username == null){
         return res.status(400).json("Por gentileza insira o username");
     }
     
-    if(!password){
+    if(password==null){
         return res.status(400).json("Por gentileza insira o password");
     }
     
-    if(!email){
+    if(email==null){
         return res.status(400).json("Por gentileza insira o email");
     }
 
