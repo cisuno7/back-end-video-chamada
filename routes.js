214eb64744bd26ccc66bd3abe243be10db500c04
Gabriel Pedro Pereira Torres <gabriel.torres@renova.com.br>
2023-05-16 10:00:37 -0300

    incluido a rota upload na api

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const firebase = require('./src/Database/db');
-require('firebase/compat/app')
-
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
 
 const route = express();
+
+// Middleware para processar o upload de arquivos
+route.use(fileUpload());
 
 // Rota para listar todos os usuários
 route.get('/users', (req, res) => {
@@ -67,6 +70,53 @@ route.post('/auth', (req, res) => {
             res.status(500).send('Erro ao autenticar usuário');
         });
 });
+route.post('/upload', (req, res) => {
+    if (!req.files || !req.files.photo) {
+      res.status(400).send('Nenhuma foto encontrada');
+      return;
+    }
+  
+    const photo = req.files.photo;
+  
+    // Lê o arquivo da foto
+    fs.readFile(photo.tempFilePath, (error, data) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Erro ao ler a foto');
+        return;
+      }
+  
+      // Converte a foto em base64
+      const base64Photo = data.toString('base64');
+  
+      // Salva a foto no banco de dados
+      firebase.collection('fotos').add({
+        photo: base64Photo, // Salva a foto no campo 'photo'
+      })
+        .then(() => {
+          res.status(201).send('Foto enviada com sucesso');
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Erro ao enviar a foto');
+        });
+    });
+  });
+  
+  
+  module.exports = route;
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
 
 
 module.exports = route;
